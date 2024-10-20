40f47dbf230309c59e8a3a2a2182aa436c933b72
robertoDCJ <robertox70@hotmail.com>
2024-10-20 16:19:53 -0300

    Feat: Header Component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './BannerImage'
 export { default as FormComponent } from './FormComponent'
+export { default as Header } from './Header'
 export * from './Logo'
 export * from './StyledButton'
 export * from './StyledInput'
