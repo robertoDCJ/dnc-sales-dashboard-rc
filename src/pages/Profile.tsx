1fd945994d6c764a4e626de3fee186aff74e7865
robertoDCJ <robertox70@hotmail.com>
2024-10-22 20:04:50 -0300

    Feat: Switch Theme Logic

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,19 @@
-import { Header } from '@/components'
+import { CardComponent, Header, StyledButton } from '@/components'
+import { AppThemeContext } from '@/contexts/AppThemeContext'
+import { useContext } from 'react'
 
 function Profile() {
+  const themeContext = useContext(AppThemeContext)
+
   return (
     <>
       <Header />
-      <h1>Profile</h1>
+      <CardComponent>
+        <StyledButton className="primary" onClick={themeContext?.toggleTheme}>
+          Trocar para tema{' '}
+          {themeContext?.appTheme === 'light' ? 'escuro' : 'claro'}
+        </StyledButton>
+      </CardComponent>
     </>
   )
 }
