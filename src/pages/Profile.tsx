6c9c7b8b80d76e0d80674e61cdaf40075a105500
robertoDCJ <robertox70@hotmail.com>
2024-11-09 17:34:40 -0300

    Feat: Logout Flow

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,10 @@
-import { CardComponent, Header, StyledButton } from '@/components'
 import { AppThemeContext } from '@/contexts/AppThemeContext'
 import { useContext } from 'react'
+
+// COMPONENTS
+import { CardComponent, Header, StyledButton, StyledH2 } from '@/components'
+import { logout } from '@/services'
+import { Container, Grid } from '@mui/material'
 
 function Profile() {
   const themeContext = useContext(AppThemeContext)
@@ -8,12 +12,28 @@ function Profile() {
   return (
     <>
       <Header />
-      <CardComponent>
-        <StyledButton className="primary" onClick={themeContext?.toggleTheme}>
-          Trocar para tema{' '}
-          {themeContext?.appTheme === 'light' ? 'escuro' : 'claro'}
-        </StyledButton>
-      </CardComponent>
+      <Container className="mb-2" maxWidth="lg">
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6}>
+            <CardComponent>Seus dados...</CardComponent>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Definições de conta</StyledH2>
+              <StyledButton
+                className="primary mb-1"
+                onClick={themeContext?.toggleTheme}
+              >
+                Trocar para tema{' '}
+                {themeContext?.appTheme === 'light' ? 'escuro' : 'claro'}
+              </StyledButton>
+              <StyledButton className="alert" onClick={logout}>
+                Logout
+              </StyledButton>
+            </CardComponent>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   )
 }
--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,3 +1,4 @@
+import { pxToRem } from '@/utils'
 import { createGlobalStyle, DefaultTheme } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
@@ -12,5 +13,13 @@ export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
     h1, h2, p, ul, li, figure {
         margin:0;
         padding: 0;
+    }
+
+    .mb-1 {
+    margin-bottom: ${pxToRem(16)};
+    }
+
+    .mb-2 {
+    margin-bottom: ${pxToRem(32)};
     }
 `
