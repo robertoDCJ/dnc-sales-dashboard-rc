d76be09e31ee36c222380815c2b861a4381d877a
robertoDCJ <robertox70@hotmail.com>
2024-11-13 20:32:33 -0300

    Feat: Create profile flow

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,5 @@
+import Cookies from 'js-cookie'
+import { jwtDecode } from 'jwt-decode'
 import { ChangeEvent, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -19,11 +21,16 @@ import { jwtExpirationDateConverter, pxToRem } from '@/utils'
 
 // TYPES
 import { DecodedJWT, LoginData, LoginPostData, MessageProps } from '@/types'
-import Cookies from 'js-cookie'
-import { jwtDecode } from 'jwt-decode'
+
+// REDUX
+import { RootState } from '@/redux'
+import { useSelector } from 'react-redux'
 
 function Login() {
   const navigate = useNavigate()
+  const { email, message } = useSelector(
+    (state: RootState) => state.createProfile
+  )
   const inputs = [
     { type: 'email', placeholder: 'Email' },
     { type: 'password', placeholder: 'Senha' },
@@ -35,7 +42,7 @@ function Login() {
   const { formValues, formValid, handleChange } = useFormValidation(inputs)
 
   const handleMessage = (): MessageProps => {
-    if (!error) return { msg: '', type: 'success' }
+    if (!error) return { msg: message ?? '', type: 'success' }
     switch (error) {
       case 401:
         return {
@@ -68,6 +75,12 @@ function Login() {
     }
     if (Cookies.get('Authorization')) navigate('/home')
   }, [data, navigate])
+
+  useEffect(() => {
+    if (email) {
+      handleChange(0, email)
+    }
+  }, [email])
 
   return (
     <>
